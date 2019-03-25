6ea6d216ac1c406c74eba02d3d8d9b141aabc01e
wwasilewski <w.wasilewski@gmail.com>
2019-03-25 14:33:05 +0100

    adding modality to 3rd window (Modality.APPLICATION_MODAL)

--- src/main/Window2.java
+++ src/main/Window2.java
@@ -3,6 +3,7 @@ package main;
 import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
 import javafx.scene.Scene;
+import javafx.stage.Modality;
 import javafx.stage.Stage;
 
 import java.io.IOException;
@@ -22,14 +23,17 @@ public class Window2 implements Initializable {
 
     }
 
+    @SuppressWarnings("Duplicates") //bajer zeby nie pokazywalo duplicate code
     public void openWindow2() throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader();
         fxmlLoader.setLocation(getClass().getResource("fxmlfiles/window2.fxml"));
 
-        @SuppressWarnings("Duplicates") //bajer zeby nie pokazywalo duplicate code
-                Scene scene = new Scene(fxmlLoader.load());
+        Scene scene = new Scene(fxmlLoader.load());
         window2Stage = new Stage();
         window2Stage.setTitle("Window 2");
+
+        //by okno bylo modalne (nie mozna nic zrobic w apce dopoki sie go nie zamknie)
+        window2Stage.initModality(Modality.APPLICATION_MODAL);
         window2Stage.setResizable(false);
         window2Stage.setScene(scene);
         window2Stage.show();
